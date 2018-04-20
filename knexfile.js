da3a79970ce0ad2fa227451b1bd5f3cffc011731
Jacob Crane <jacobcrane.dev@gmail.com>
2018-04-20 14:46:49 -0600

    finished

---
+++ knexfile.js
@@ -0,0 +1,18 @@
+// Update with your config settings.
+require('dotenv').load()
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'postgres:///resolutions'
+  },
+
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+  }
+
+};
+ 
